3a7d3ae3d708ad8ef113b7d378139d2a5c08a54b
dkw5877 <dkw5877@gmail.com>
2014-04-08 14:52:37 -0500

    added tap gesture to photo, begin comment feature

--- MyInstagram/MainViewController.m
+++ MyInstagram/MainViewController.m
@@ -24,27 +24,89 @@
 {
     [super viewDidLoad];
     
-    PFObject *user1 = [PFObject objectWithClassName:@"User"];
-    NSData *fileData = UIImagePNGRepresentation([UIImage imageNamed:@"chicagosunset.jpg"]);
-    PFFile *file = [PFFile fileWithName:@"chicagosunset.png" data:fileData];
-    NSLog(@"file %@",file);
+    //assign an automated user
+    [PFUser enableAutomaticUser];
+    
+    //[self CreateRandomPhotosForUsers];
+    
+    [self queryParseForUserPhotos];
 
-    user1[@"username"] = @"Jay";
-    user1[@"photo"] = file;
+}
 
+
+- (void)queryParseForUserPhotos
+{
+    //set the object class to look for
+    PFQuery *query = [PFQuery queryWithClassName:@"User"];
     
-    PFObject *user2 = [PFObject objectWithClassName:@"User"];
-    NSData *fileData2 = UIImagePNGRepresentation([UIImage imageNamed:@"armitagemorning.jpg"]);
-    PFFile *file2 = [PFFile fileWithName:@"armitagemorning.png" data:fileData2];
-    user2[@"username"] = @"Albus";
-    user2[@"photo"] = file2;
+    //set the field to search and the value of the field
+    [query whereKey:@"username" equalTo:@"jay"]; //replace with current user or !current user for search
     
-    users = [[NSArray alloc]initWithObjects:user1,user2, nil];
+    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        if (!error)
+        {
+            // The find succeeded.
+            NSLog(@"Successfully retrieved %d photos.", objects.count);
+            
+            //assign result array of photos to our array
+            users = objects;
+            [myCollectionView reloadData];
+        }
+        else
+        {
+            // Log details of the failure
+            NSLog(@"Error: %@ %@", error, [error userInfo]);
+        }
+    }];
+}
+
+
+- (IBAction)onPhotoDoubleTapped:(UITapGestureRecognizer *)sender
+{
+    NSLog(@"tap gesture recognized");
     
-    //[user1 saveInBackground];
-    //[user2 saveInBackground];
+    //create an activity and associate to user
+    PFObject* like = [PFObject objectWithClassName:@"Activity"];
+    like[@"fromUser"] = [PFUser currentUser];
+    like[@"type"] = @"like";
+    like[@"photo"] = nil;
+    like[@"content"]= [NSString stringWithFormat:@"%@ liked your photo",[PFUser currentUser]];
+    
+    like[@"fromUser"] = [PFUser currentUser];
     
     
+}
+
+-(void)CreateRandomPhotosForUsers
+{
+
+
+
+//    for (int i=0; i < 20; i++)
+//    {
+//        int width = 270 + arc4random()%100;
+//        int height = 270 + arc4random()%100;
+//        
+//        NSString* urlString =[NSString stringWithFormat:@"http://placekitten.com/%d/%d",width,height];
+//        
+//        NSURL* url = [NSURL URLWithString:urlString];
+//        
+//        NSData* data = [NSData dataWithContentsOfURL:url];
+//        
+//        PFObject* object = [PFObject objectWithClassName:@"User"];
+//        PFFile* file = [PFFile fileWithData:data];
+//        
+//        [object setObject:file forKey:@"photo"];
+//        [object setObject:@"dennis" forKey:@"username"];
+//        [object saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error)
+//        {
+//            if (error)
+//            {
+//                NSLog(@"%@", [error userInfo]);
+//            }
+//        }];
+//    }
+
 }
 
 -(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
@@ -52,20 +114,26 @@
     return users.count;
 }
 
+
 -(UICollectionViewCell*)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
 {
+    
+    //create tap gesture recognizer
+    UITapGestureRecognizer *gestureRecognizer = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(onPhotoDoubleTapped:)];
+    
+    //set number of taps required
+    gestureRecognizer.numberOfTapsRequired = 2;
+    
+    
+    //add tap gesture to view
+    [self.view addGestureRecognizer:gestureRecognizer];
+   
+    
     ImageCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"imageCellIdentifier" forIndexPath:indexPath];
     PFObject* user = users[indexPath.row];
-    //NSLog(@"user %@",user);
     PFFile* file = user[@"photo"];
-    NSLog(@"file %@",file);
-    
     NSData* data = [file getData];
-    NSLog(@"data %@",data);
-    
     cell.imageView.image = [UIImage imageWithData:data];
-    
-    //NSLog(@"This is an %@", cell.imageView.image);
     return cell;
 }
 

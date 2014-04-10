44ad8dd98ccbcc9a281add7aa80281f6d6f88be3
dkw5877 <dkw5877@gmail.com>
2014-04-10 11:58:02 -0500

    followers view controller table cell config

--- MyInstagram/FollowingViewController.m
+++ MyInstagram/FollowingViewController.m
@@ -36,8 +36,6 @@
 
 }
 
-
-
 - (PFQuery *)queryForTable
 {
     //set the object class to look for activites
@@ -51,16 +49,22 @@
     [activityQuery includeKey:@"photo"];
     
     PFQuery *photoQuery = [PFQuery queryWithClassName:@"Photo"];
-    
     [photoQuery whereKey:@"image" matchesKey:@"photo" inQuery:activityQuery];
-    
     return photoQuery;
 }
 
-
-    [super viewDidLoad];
-
+- (PFTableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath object:(PFObject *)object
+{
+    PFTableViewCell* cell = [super tableView:tableView cellForRowAtIndexPath:indexPath object:object];
     
+    cell.imageView.file = [object objectForKey:@"image"];
+    
+    [cell.imageView loadInBackground:^(UIImage *image, NSError *error)
+    {
+        cell.imageView.image = image;
+    }];
+    
+    return cell;
 }
 
 
@@ -88,48 +92,6 @@
 //             for (PFObject *activity in objects)
 //             {
 //                 // This does not require a network access.
-//                 PFObject *photo = activity[@"photo"];
-//                 [photos addObject:photo];
-//                 
-//                 NSLog(@"retrieved related photo: %@", photo);
-//             }
-//             
-//             [followingTableView reloadData];
-//         }
-//         else
-//         {
-//             // Log details of the failure
-//             NSLog(@"Error: %@ %@", error, [error userInfo]);
-//         }
-//     }];
-//    
-//}
-
-
-
-////temp method to get photos liked by a user
-//-(void)getFollowingPhotos
-//{
-//    NSLog(@"get following photos");
-//    
-//    //set the object class to look for activites
-//    PFQuery *activityQuery = [PFQuery queryWithClassName:@"Activity"];
-//    
-//    //set the field to search and the value of the field
-//    [activityQuery whereKey:@"ActivityType" equalTo:@"follow"];
-//    [activityQuery whereKey:@"fromUser" equalTo:[PFUser currentUser]];
-//    
-//    [activityQuery findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error)
-//     {
-//         if (!error)
-//         {
-//             // The find succeeded.
-//             NSLog(@"Successfully retrieved %d following photos.", objects.count);
-//             
-//             //loop over all the activities and get the photo for each one,
-//             //then add it to the photo array
-//             for (PFObject *activity in objects)
-//             {
 //                 PFObject *photo = activity[@"photo"];
 //                 [photos addObject:photo];
 //                 
